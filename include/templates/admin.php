e3d90d5f3e004f18ab6249adf3bb8c5afd5cd9b2
Max Chan <max@maxchan.info>
2025-08-23 17:57:13 +0000

    Relocate templates, part 2

--- include/templates/admin.php
+++ include/templates/admin.php
@@ -1,23 +1,23 @@
 <?php
-require_once dirname(__FILE__) . "/../include/admin.php";
-require_once dirname(__FILE__) . "/../include/config.php";
-require_once dirname(__FILE__) . "/../include/utils.php";
+require_once dirname(__FILE__) . "/../admin.php";
+require_once dirname(__FILE__) . "/../config.php";
+require_once dirname(__FILE__) . "/../utils.php";
 ?>
 <!DOCTYPE html>
 <html lang="<?= get_current_language() ?>">
     <head>
         <meta charset="utf-8" />
-        <title><?= $CONFIG['SITENAME'] ?> - <?= $TITLE ?></title>
+        <title><?= L::SITENAME ?> - <?= $TITLE ?></title>
         <?php if (!empty($HEADERS)) echo $HEADERS ?>
     </head>
     <body>
         <header>
-            <div><?= $CONFIG['SITENAME'] ?> - <?= $TITLE ?></div>
+            <div><?= L::SITENAME ?> - <?= $TITLE ?></div>
             <div><?= auth_link() ?></div>
         </header>
         <main>
             <?= $CONTENTS ?>
         </main>
-        <footer>Copyright &copy; <?= date('Y') ?> <a href="/"><?= $CONFIG['AUTHOR'] ?></a> - Powered by <a href="https://github.com/SushiBits/miniblog">miniblog</a></footer>
+        <footer>Copyright &copy; <?= date('Y') ?> <a href="/"><?= L::AUTHOR ?></a> - Powered by <a href="https://github.com/SushiBits/miniblog">miniblog</a></footer>
     </body>
 </html>
