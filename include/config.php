a9867cf0b26d836a7fc584f40c5d4b976f832c29
Max Chan <max@maxchan.info>
2025-08-23 18:31:44 +0000

    Further refining i18n support

--- include/config.php
+++ include/config.php
@@ -2,5 +2,6 @@
 $CONFIG = array();
 
 $CONFIG['DEFAULTLANG'] = 'en';
+$CONFIG['HOSTNAME'] = 'lab.maxchan.xyz';
 $CONFIG['PAGECOUNT'] = 25;
 $CONFIG['DATE_FORMAT'] = 'Y-m-d H:m:s';
--- include/utils.php
+++ include/utils.php
@@ -14,7 +14,7 @@ if (!empty($hostlang))
 $I18N->init();
 
 if ($hostlang != get_current_language()) {
-    header('Location: ' . (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . get_current_language() . "." . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], true, 307);
+    header('Location: ' . (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . get_current_language() . "." . $GLOBALS['CONFIG']['HOSTNAME'] . $_SERVER['REQUEST_URI'], true, 307);
     exit;
 }
 
@@ -23,14 +23,37 @@ function get_available_languages(): array {
     return array_map(function(string $name): string { return basename($name, ".ini"); }, $languages);
 }
 
-function get_host_language(): string|null {
-    $host_language = reset(explode('.', $_SERVER['HTTP_HOST']));
-    if (in_array($host_language, get_available_languages()))
-        return $host_language;
+function get_host_language(): ?string {
+    $host = $_SERVER['HTTP_HOST'];
+    if (!str_ends_with($host, $GLOBALS['CONFIG']['HOSTNAME']))
+        return null;
+
+    $host = substr($host, 0, -strlen($GLOBALS['CONFIG']['HOSTNAME']));
+    if (str_ends_with($host, '.'))
+        $host = substr($host, 0, -1);
+
+    if (in_array($host, get_available_languages()))
+        return $host;
     else
         return null;
 }
 
 function get_current_language(): string {
     return $GLOBALS['I18N']->getAppliedLang();
+}
+
+function get_language_name(string $lang): string {
+    static $langnames = [];
+    if (isset($langnames[$lang]))
+        return $langnames[$lang];
+
+    $langfile = $GLOBALS['LANGDIR'] . DIRECTORY_SEPARATOR . $lang . '.ini';
+    if (!file_exists($langfile) || !is_readable($langfile))
+        return $langnames[$lang] = $lang;
+
+    $langdoc = parse_ini_file($langfile, true);
+    if (empty($langdoc))
+        return $langnames[$lang] = $lang;
+    
+    return $langnames[$lang] = $langdoc['LANGNAME'] ?? $lang;
 }
