a35baee2bcb07faa35505d0f54d48158734a800d
Max Chan <max@maxchan.info>
2025-08-26 15:57:00 +0000

    Partially stylized the admin page article list page.

--- admin/index.php
+++ admin/index.php
@@ -11,6 +11,61 @@ $articles = Article::articles();
 
 ob_start();
 ?>
+<style>
+table, nav.operations {
+    background-color: white;
+    border-radius: 6px;
+    border-color: #333333;
+    border-width: 1px;
+    border-style: solid;
+    max-width: 600px;
+    margin: 12px auto 12px auto;
+    width: 100%;
+    color: #333333;
+    overflow: hidden;
+}
+
+nav.operations {
+    display: flex;
+    flex-direction: row;
+    align-items: first baseline;
+    vertical-align: middle;
+    padding: 0px;
+}
+
+nav.operations a {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 36px;
+    padding-left: 12px;
+    padding-right: 12px;
+    color: #333333;
+    text-decoration: none;
+}
+
+nav.operations a span {
+    margin: 6px;
+}
+
+nav.operations a:hover {
+    color: white;
+    background-color: #333333;
+}
+
+nav.operations a:hover img {
+    filter: invert(100%);
+}
+</style>
+<?php
+$HEADERS = ob_get_clean();
+
+ob_start();
+?>
+<nav class="operations">
+    <a href="/admin/edit.php"><img src="/images/ic_public_add.svg" alt="<?= L::Admin_NEW_ARTICLE ?>" /><span><?= L::Admin_NEW_ARTICLE ?></span></a>
+</nav>
 <table>
     <tr>
         <th><?= L::Article_TITLE ?></th>
--- admin/login.php
+++ admin/login.php
@@ -26,6 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         exit;
     }
 }
+
 ob_start();
 ?>
 <style>
